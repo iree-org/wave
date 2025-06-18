cf86c86e908f111203d9996d45d03bbb089d40f0
nithinsubbiah <nithinsubbiah@gmail.com>
2025-06-18 20:43:54 +0000

    Extract read and write info in the pass
    Signed-off-by: nithinsubbiah <nithinsubbiah@gmail.com>
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- iree/turbine/kernel/ops/wave_ops.py
+++ iree/turbine/kernel/ops/wave_ops.py
@@ -276,8 +276,15 @@ def select(
 
 
 def gather_to_lds(
-    src: fx.Node,
-    dst: fx.Node,
+    src: Memory,
+    src_idx: dict[IndexSymbol, IndexSequence],
+    src_type: DataType,
+    dst: Memory,
+    dst_idx: dict[IndexSymbol, IndexSequence],
+    dst_type: DataType,
+    src_mapping: Optional[IndexMapping] = None,
+    dst_mapping: Optional[IndexMapping] = None,
+    elements_per_thread: Optional[IndexExpr | int] = None,
 ):
     ...
 
@@ -2509,5 +2516,12 @@ class GatherToLDS(CustomOp):
     and the destination node points to shared memory.
     """
 
-    src: fx.Node
-    dst: fx.Node
+    src: Memory
+    src_idx: dict[IndexSymbol, IndexSequence]
+    src_type: DataType
+    dst: Memory
+    dst_idx: dict[IndexSymbol, IndexSequence]
+    dst_type: DataType
+    src_mapping: Optional[IndexMapping]
+    dst_mapping: Optional[IndexMapping]
+    elements_per_thread: Optional[IndexExpr | int]
