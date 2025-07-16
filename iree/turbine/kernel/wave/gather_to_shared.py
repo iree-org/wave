4e3446b925852917d8127bf0307964496d8e748e
Ivan Butygin <ivan.butygin@gmail.com>
2025-07-16 22:43:54 +0200

    ratio
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- iree/turbine/kernel/wave/gather_to_shared.py
+++ iree/turbine/kernel/wave/gather_to_shared.py
@@ -225,7 +225,7 @@ def gather_to_shared(trace: CapturedTrace, constraints: list[Constraint]):
         new_writes = defaultdict(list)
         for i in range(expected_number_of_loads):
             nd_index = delinearize_index(
-                thread_id + i * elements_per_wave, symbolic_shape_adjusted
+                thread_id + i * elements_per_wave // ratio, symbolic_shape_adjusted
             )
             logger.info(f"nd_index={nd_index}")
             write_index = {}
