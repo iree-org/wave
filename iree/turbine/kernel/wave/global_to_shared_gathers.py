iree-org/iree-turbine

[Wave] Add support for direct global load to lds
Signed-off-by: nithinsubbiah <nithinsubbiah@gmail.com>

iree/turbine/kernel/wave/global_to_shared_gathers.py
@@ -22,12 +22,14 @@ from copy import deepcopy
 from .utils.symbol_utils import subs_idxc
 from .utils.general_utils import is_gather
 from .minimize_global_loads import (
-    has_write_shared_user,
     construct_min_global_access_pattern,
     materialize_shape,
     identify_optimizable_loads,
     update_write_dependencies,
     SharedReadMetadata,
+)
+from .utils.graph_utils import (
+    has_write_shared_user,
 )
 
 """

iree-org/wave

Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

iree/turbine/kernel/wave/global_to_shared_gathers.py
@@ -22,12 +22,14 @@ from copy import deepcopy
 from .utils.symbol_utils import subs_idxc
 from .utils.general_utils import is_gather
 from .minimize_global_loads import (
-    has_write_shared_user,
     construct_min_global_access_pattern,
     materialize_shape,
     identify_optimizable_loads,
     update_write_dependencies,
     SharedReadMetadata,
+)
+from .utils.general_utils import (
+    has_write_shared_user,
 )
 
 """