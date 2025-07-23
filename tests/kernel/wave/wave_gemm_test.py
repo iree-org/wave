6401a478b2952f48860e1e7096792d7bf6f2a4b5
Ivan Butygin <ivan.butygin@gmail.com>
2025-07-23 21:02:04 +0200

    xfail
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- tests/kernel/wave/wave_gemm_test.py
+++ tests/kernel/wave/wave_gemm_test.py
@@ -185,8 +185,8 @@ _global_to_lds_shapes = [(17, 23, 32), (15, 13, 4)]
     [
         SchedulingType.NONE,
         SchedulingType.PREFETCH,
-        SchedulingType.MODULO,
-        SchedulingType.MODULO_MULTI_BUFFERED,
+        _xfail(SchedulingType.MODULO),
+        _xfail(SchedulingType.MODULO_MULTI_BUFFERED),
     ],
 )
 @pytest.mark.parametrize(
