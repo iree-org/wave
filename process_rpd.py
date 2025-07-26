1b32c5c0efd1b300ae7e8f928161e74001c5ad6a
Drew Broch <badgerbroch@gmail.com>
2025-07-26 01:11:42 +0000

    Ran pre-commit and got rid of extra files
    Signed-off-by: Drew Broch <badgerbroch@gmail.com>

--- process_rpd.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-from sys import argv
-
-conn = sqlite3.connect(argv[1])
-df_top = pd.read_sql_query("SELECT * from top", conn)
-df_busy = pd.read_sql_query("SELECT * from busy", conn)
-conn.close()
-print(df_top.head(100))
