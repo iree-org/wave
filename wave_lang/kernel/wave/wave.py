c2666fcdb93567f3409724cb7f853bc1a9a3ce26
Ashay Rane <ashay@users.noreply.github.com>
2025-08-19 15:33:25 -0500

    Add support for hardware transpose operation
    This patch enables the emission of `amdgpu.transpose_load` operations on
    gfx950 machines based on the underlying read operation.  We deliberately
    decouple the transpose load op generation from the use of MFMA
    instructions so that regardless of whether the results of the read
    operation are fed to MFMA operations, they are rewritten to use the
    transpose operation.  This patch also updates the lit tests to match the
    serialization after adding the `transpose` field to the `ReadOp` type.
    
    Signed-off-by: Ashay Rane <ashay@users.noreply.github.com>

--- wave_lang/kernel/wave/wave.py
+++ wave_lang/kernel/wave/wave.py
@@ -73,6 +73,7 @@ from .expansion.expansion import add_get_results, expand_graph
 from .gather_to_shared import gather_to_shared, gather_to_shared_swizzling
 from .generate_bounds_exprs import generate_bounds_exprs
 from .global_to_shared_gathers import global_to_shared_gathers
+from .hardware_transpose import mark_hardware_transpose_candidates
 from .hoisting import hoist_loop_invariant_ops
 from .in_thread_transpose import in_thread_transpose
 from .memory_analysis.minimize_shared_allocs import minimize_shared_allocs
@@ -759,6 +760,9 @@ class LaunchableWave(Launchable):
                 partial(in_thread_transpose, trace, self.constraints, options),
                 partial(global_to_shared_gathers, trace, self.constraints),
                 partial(minimize_global_loads, trace, self.constraints),
+                partial(
+                    mark_hardware_transpose_candidates, trace, self.constraints, options
+                ),
             ]
         graph_passes += [
             partial(apply_shared_memory_indexing_corrections, trace, self.constraints),
