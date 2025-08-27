2cc0683bf06696ea53db2beac4ccb5103ed288ed
William G Hatch <william@hatch.uno>
2025-08-27 16:37:49 +0000

    implement broadcasting for SelectOp
    Motivated by my topk implementation which needs broadcasted selection.
    Adds broadcasting for SelectOp alongside broadcasting for binary ops.
    
    Signed-off-by: William G Hatch <william@hatch.uno>

--- wave_lang/kernel/wave/codegen/handlers.py
+++ wave_lang/kernel/wave/codegen/handlers.py
@@ -19,7 +19,6 @@ from wave_lang.aot.support.ir_utils import (
     _is_signed_or_signless_type,
     get_conversion_op,
 )
-from wave_lang.kernel._support.shaped_type import ShapedType
 
 # TK infrastructure imports.
 from wave_lang.kernel.lang.global_symbols import *
@@ -557,11 +556,12 @@ def handle_atomic_op(op):
 
 
 def get_rank(mlir_type):
-    if not isinstance(mlir_type, ShapedType):
+    if hasattr(mlir_type, "rank"):
+        return mlir_type.rank
+    else:
         # Not 0 because vector<f32> is rank 0, and in theory,
         # is broadcastable from pure scalar.
         return -1
-    return mlir_type.rank
 
 
 def handle_binary_op(op):
