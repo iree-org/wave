7b6789035c20237bd4d10ff95ab31bdfbb9cd5a7
Ashay Rane <ashay@users.noreply.github.com>
2025-09-09 16:12:30 -0500

    Skip the in-thread-transpose pass when compiling for gfx95X chips
    We do this because the hardware-transpose pass (that we'll add in the
    next commit) will supersede the compilation on MI350+ machines.

--- wave_lang/kernel/wave/wave.py
+++ wave_lang/kernel/wave/wave.py
@@ -756,7 +756,7 @@ class LaunchableWave(Launchable):
                 partial(hoist_loop_invariant_ops, trace, self.constraints),
                 partial(gather_to_shared, trace, self.constraints, options),
                 partial(gather_to_shared_swizzling, trace, self.constraints, options),
-                partial(in_thread_transpose, trace, self.constraints),
+                partial(in_thread_transpose, trace, self.constraints, options),
                 partial(global_to_shared_gathers, trace, self.constraints),
                 partial(minimize_global_loads, trace, self.constraints),
             ]
