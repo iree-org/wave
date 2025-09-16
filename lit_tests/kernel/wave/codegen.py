eea51627a10b96fbf9aecbdbc77f6775ff43aad6
William G Hatch <william@hatch.uno>
2025-09-16 12:41:02 -0600

    change codegen.py to have location dropping to still pass
    Signed-off-by: William G Hatch <william@hatch.uno>

--- lit_tests/kernel/wave/codegen.py
+++ lit_tests/kernel/wave/codegen.py
@@ -168,7 +168,8 @@ def test_read_mapped_buffer():
         use_buffer_ops=True,
         compile_to_mlir=True,
         canonicalize=False,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.FILE_LINE_COL),
+        drop_debug_info_before_mlir=True,
     )
     read_mapped_buffer = wave_compile(options, read_mapped_buffer)
     print(read_mapped_buffer.asm)
@@ -210,7 +211,7 @@ def test_read_dynamic_3d_buffer():
         use_buffer_ops=True,
         compile_to_mlir=True,
         canonicalize=False,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     read_dynamic_buffer = wave_compile(options, read_dynamic_buffer)
     print(read_dynamic_buffer.asm)
@@ -337,7 +338,7 @@ def test_read_write_masked():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     read_write_masked = wave_compile(options, read_write_masked)
     print(read_write_masked.asm)
@@ -390,7 +391,7 @@ def test_read_write_masked_shared():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     read_write_masked_shared = wave_compile(options, read_write_masked_shared)
     print(read_write_masked_shared.asm)
@@ -1090,7 +1091,7 @@ def test_reduce_sum():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, test)
@@ -1166,7 +1167,7 @@ def test_mutliple_local_reduce_sum():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, test)
@@ -1238,7 +1239,7 @@ def test_reduction_and_elemwise():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, test)
@@ -1327,7 +1328,7 @@ def test_tiled_reduce_max():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, test)
@@ -1418,7 +1419,7 @@ def test_tiled_reduce_min():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, tiled_reduce_min)
@@ -1509,7 +1510,7 @@ def test_tiled_reduce_min_unaligned():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, tiled_reduce_min_unaligned)
@@ -1590,7 +1591,7 @@ def test_multiple_reduction_iv():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, test)
@@ -1690,7 +1691,7 @@ def test_reduce_propagate_broadcast():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test = wave_compile(options, test)
@@ -1764,7 +1765,7 @@ def test_block_reduce_sum():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     test_block_reduce_sum = wave_compile(options, test_block_reduce_sum)
@@ -1841,7 +1842,7 @@ def test_explicit_broadcast():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     explicit_broadcast = wave_compile(options, explicit_broadcast)
@@ -1965,7 +1966,7 @@ def test_broadcast_add():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     broadcast_add = wave_compile(options, broadcast_add)
@@ -2013,7 +2014,7 @@ def test_broadcast_scaled_add():
         canonicalize=True,
         use_buffer_ops=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     options = set_default_compile_config(options)
     broadcast_scaled_add = wave_compile(options, broadcast_scaled_add)
@@ -2272,7 +2273,7 @@ def test_register_codegen_i32():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     register_codegen_i32 = wave_compile(options, register_codegen_i32)
     print(register_codegen_i32.asm)
@@ -2325,7 +2326,7 @@ def test_scalar_codegen_f32():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     scalar_codegen_f32 = wave_compile(options, scalar_codegen_f32)
     print(scalar_codegen_f32.asm)
@@ -2384,7 +2385,7 @@ def test_scalar_codegen_i32():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     scalar_codegen_i32 = wave_compile(options, scalar_codegen_i32)
     print(scalar_codegen_i32.asm)
@@ -2463,7 +2464,7 @@ def test_scalar_cond_copy():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     scalar_cond_copy = wave_compile(options, scalar_cond_copy)
     print(scalar_cond_copy.asm)
@@ -2523,7 +2524,7 @@ def test_scanop_cumsum():
         },
         canonicalize=True,
         compile_to_mlir=True,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
     scanop_cumsum = wave_compile(options, scanop_cumsum)
     print(scanop_cumsum.asm)
@@ -2613,7 +2614,7 @@ def test_atomic_min():
         use_buffer_ops=False,
         compile_to_mlir=True,
         minimize_shared_allocs=False,
-        location_capture_config=LocationCaptureConfig(level=LocationCaptureLevel.NONE),
+        drop_debug_info_before_mlir=True,
     )
 
     atomic_min_codegen = wave_compile(options, atomic_min_codegen)
