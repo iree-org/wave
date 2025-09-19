95154c9a728d8dfb979faa4e71c24d15b61ad4ea
Sanket Pandit <sanket.pandit@amd.com>
2025-09-18 21:11:41 -0700

    no async support, doesn't work when running in a loop
    Signed-off-by: Sanket Pandit <sanket.pandit@amd.com>

--- tests/kernel/wave/dist_gemm_test.py
+++ tests/kernel/wave/dist_gemm_test.py
@@ -73,6 +73,7 @@ def testDistGemm(
 
     options = WaveCompileOptions(
         subs=hyperparams,
+        iree_launch_async=False,
     )
     options = set_default_run_config(options)
     gemm = wave_compile(options, gemm)
