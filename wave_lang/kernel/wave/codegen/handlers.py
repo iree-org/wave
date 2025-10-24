9fd8fe5aafe95e7fff869b7181085d70e0482ded
Megan Kuo <mkuo@amd.com>
2025-10-24 19:21:59 +0000

    Tensor waticnt support - pending for updated iree version for rocdl tensor waitcnt support
    Signed-off-by: Megan Kuo <mkuo@amd.com>

--- wave_lang/kernel/wave/codegen/handlers.py
+++ wave_lang/kernel/wave/codegen/handlers.py
@@ -1614,12 +1614,15 @@ def waitcnt(vmcnt: int):
 @handle_op(shared_memory_barrier)
 def handle_shared_memory_barrier(emitter: WaveEmitter, node: fx.Node):
     try:
-        (wait_async_ops,) = node.args
+        (wait_async_ops, tensor_wait, ) = node.args
     except ValueError as e:
         raise ValidationError("Malformed arguments") from e
 
     if wait_async_ops:
         waitcnt(0)
+
+    if tensor_wait:
+        rocdl_d.s_wait_tensorcnt(0)
 
     amdgpu_d.lds_barrier()
 
