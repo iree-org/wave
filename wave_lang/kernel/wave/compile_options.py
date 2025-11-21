3fef6a05dd0bc5be017eaa710136bc9b882e8f53
mkuo <mkuo@amd.com>
2025-11-21 13:12:31 -0800

    Add specialization option in compile options
    Signed-off-by: mkuo <mkuo@amd.com>

--- wave_lang/kernel/wave/compile_options.py
+++ wave_lang/kernel/wave/compile_options.py
@@ -95,6 +95,7 @@ class WaveCompileOptions:
     override_schedule: Optional[str] = None
     dump_schedule: Optional[str] = None
     use_bound_check: bool = False
+    specialize: bool = False
 
     # === Print options ===
     mlir_print_ir_after_all: bool = False
