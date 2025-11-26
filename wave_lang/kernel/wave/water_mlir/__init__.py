1c547472028738c4e3ca69d18bdaf7cb28fbda91
Ivan Butygin <ivan.butygin@gmail.com>
2025-11-26 12:08:52 +0100

    license header
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- wave_lang/kernel/wave/water_mlir/__init__.py
+++ wave_lang/kernel/wave/water_mlir/__init__.py
@@ -1,3 +1,9 @@
+# Copyright 2025 The Wave Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 from importlib import import_module as _import_module
 import sys
 
