06e83de5eceefb0741bf56efe45a00fb93b585b7
Ivan Butygin <ivan.butygin@gmail.com>
2025-12-03 15:09:11 +0100

    update water-opt
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- water/tools/water-opt/water-opt.cpp
+++ water/tools/water-opt/water-opt.cpp
@@ -6,6 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
 #include "mlir/Conversion/Passes.h"
 #include "mlir/Dialect/AMDGPU/IR/AMDGPUDialect.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
@@ -14,15 +16,18 @@
 #include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
+#include "mlir/Dialect/GPU/Transforms/Passes.h"
 #include "mlir/Dialect/LLVMIR/ROCDLDialect.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
-#include "mlir/InitAllExtensions.h"
 #include "mlir/InitAllPasses.h"
-#include "mlir/Target/LLVMIR/Dialect/All.h"
+#include "mlir/Target/LLVM/ROCDL/Target.h"
+#include "mlir/Target/LLVMIR/Dialect/GPU/GPUToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/ROCDL/ROCDLToLLVMIRTranslation.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "mlir/Transforms/Passes.h"
 
@@ -42,15 +47,51 @@ int main(int argc, char **argv) {
   mlir::water::registerPasses();
   mlir::water::test::registerAllPasses();
   wave::registerPasses();
+  mlir::arith::registerArithIntRangeOptsPass();
+  mlir::registerCanonicalizerPass();
+  mlir::registerCSEPass();
+  mlir::registerLoopInvariantCodeMotionPass();
+  mlir::registerLowerAffinePass();
+  mlir::registerCompositeFixedPointPass();
+  mlir::registerConvertAMDGPUToROCDLPass();
+  mlir::registerConvertGpuOpsToROCDLOpsPass();
+  mlir::registerGpuROCDLAttachTarget();
+  mlir::registerGpuToLLVMConversionPass();
+  mlir::registerReconcileUnrealizedCastsPass();
+  mlir::registerGpuModuleToBinaryPass();
+  mlir::registerSymbolDCEPass();
 
-  mlir::registerAllPasses();
   mlir::DialectRegistry registry;
-  mlir::registerAllDialects(registry);
-  mlir::registerAllExtensions(registry);
+  registry.insert<
+      // clang-format off
+      mlir::LLVM::LLVMDialect,
+      mlir::ROCDL::ROCDLDialect,
+      mlir::affine::AffineDialect,
+      mlir::amdgpu::AMDGPUDialect,
+      mlir::arith::ArithDialect,
+      mlir::cf::ControlFlowDialect,
+      mlir::func::FuncDialect,
+      mlir::gpu::GPUDialect,
+      mlir::memref::MemRefDialect,
+      mlir::scf::SCFDialect,
+      mlir::vector::VectorDialect,
+      wave::WaveDialect
+      // clang-format on
+      >();
 
-  mlir::registerAllGPUToLLVMIRTranslations(registry);
+  mlir::arith::registerConvertArithToLLVMInterface(registry);
+  mlir::cf::registerConvertControlFlowToLLVMInterface(registry);
+  mlir::registerConvertFuncToLLVMInterface(registry);
+  mlir::registerConvertMemRefToLLVMInterface(registry);
+  mlir::ub::registerConvertUBToLLVMInterface(registry);
+  mlir::vector::registerConvertVectorToLLVMInterface(registry);
 
-  registry.insert<wave::WaveDialect>();
+  mlir::ROCDL::registerROCDLTargetInterfaceExternalModels(registry);
+
+  mlir::registerGPUDialectTranslation(registry);
+  mlir::registerLLVMDialectTranslation(registry);
+  mlir::registerROCDLDialectTranslation(registry);
+
   mlir::water::test::registerWaterTestDialect(registry);
 
   return mlir::asMainReturnCode(
