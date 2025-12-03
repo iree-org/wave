aa546f4baf90b855a2e1345ffdc67378f9ce222f
Ivan Butygin <ivan.butygin@gmail.com>
2025-12-03 14:03:53 +0100

    fixes
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- wave_lang/kernel/wave/execution_engine/execution_engine.py
+++ wave_lang/kernel/wave/execution_engine/execution_engine.py
@@ -135,6 +135,8 @@ def _create_options_from_env() -> "ExecutionEngineOptions":
         WAVE_ENABLE_OBJECT_CACHE: Enable object cache (default: 0)
         WAVE_ENABLE_GDB_LISTENER: Enable GDB notification listener (default: 0)
         WAVE_ENABLE_PERF_LISTENER: Enable Perf notification listener (default: 0)
+        WAVE_JIT_OPT_LEVEL: JIT optimization level 0-3 (default: 3)
+                            0=None/O0, 1=Less/O1, 2=Default/O2, 3=Aggressive/O3
 
     Returns:
         ExecutionEngineOptions configured from environment
@@ -156,6 +158,10 @@ def _create_options_from_env() -> "ExecutionEngineOptions":
     options.enable_perf_notification_listener = _env_enabled(
         "WAVE_ENABLE_PERF_LISTENER"
     )
+
+    # Set JIT optimization level (default: 3/Aggressive/O3)
+    jit_opt_level = os.environ.get("WAVE_JIT_OPT_LEVEL", "3")
+    options.set_jit_code_gen_opt_level(int(jit_opt_level))
 
     return options
 
