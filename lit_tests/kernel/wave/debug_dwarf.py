ca1021425bbb861b843bcdc28124da2b9f55adf7
William G Hatch <william@hatch.uno>
2025-12-05 16:36:28 -0700

    Revert "remove unnecessary .value"
    This reverts commit f5d31a25ee07404dcc4284a739d3a84b56f91fb9.
    
    Signed-off-by: William G Hatch <william@hatch.uno>

--- lit_tests/kernel/wave/debug_dwarf.py
+++ lit_tests/kernel/wave/debug_dwarf.py
@@ -73,7 +73,7 @@ def test_debug_dwarf():
             BLOCK_M: 32,
             BLOCK_N: 32,
             BLOCK_K: 16,
-            ADDRESS_SPACE: tkl.AddressSpace.SHARED_MEMORY,
+            ADDRESS_SPACE: tkl.AddressSpace.SHARED_MEMORY.value,
         },
         canonicalize=True,
         compile_to_mlir=False,
