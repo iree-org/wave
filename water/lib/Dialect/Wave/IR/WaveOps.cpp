d29836fe456c4941ee12cb5e11acc47a5e2aacb6
Alex Zinenko <git@ozinenko.com>
2026-02-16 12:25:13 +0100

    add lowering
    via direct expansion of affine expressions
    
    Signed-off-by: Alex Zinenko <git@ozinenko.com>

--- water/lib/Dialect/Wave/IR/WaveOps.cpp
+++ water/lib/Dialect/Wave/IR/WaveOps.cpp
@@ -1963,6 +1963,9 @@ LogicalResult wave::ApplyExprOp::verify() {
     return emitOpError("expression must produce exactly one result, but got ")
            << numResults;
 
+  if (!isa<IntegerType>(getElementType(getResult().getType())))
+    return emitOpError() << "operates on integers only";
+
   llvm::BitVector usedOperandAttrs(getArguments().size());
   for (Attribute sym : getExpr().getSymbols()) {
     if (auto operand = dyn_cast<WaveOperandAttr>(sym)) {
