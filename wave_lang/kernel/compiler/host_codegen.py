a9b84ac5209083336bbc1a053bea87f8449785e0
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2026-02-17 00:18:52 +0000

    Pass in dynamic stride arguments at compile & runtime
    Signed-off-by: Surya Jasper <45545431+suryajasper@users.noreply.github.com>

--- wave_lang/kernel/compiler/host_codegen.py
+++ wave_lang/kernel/compiler/host_codegen.py
@@ -94,6 +94,7 @@ def isolated_test_call(
     async_dispatch: bool = False,
     device_layout: Optional[Grid] = None,
     device_constraints: Optional[list[DeviceConstraint]] = None,
+    use_dynamic_strides: bool = False,
 ):
     with InsertionPoint(mb.body_block), Location.unknown():
 
@@ -118,6 +119,16 @@ def isolated_test_call(
 
                 arg_dim_mapping[dim_symbol] = (arg_idx, dim_idx)
 
+        # Generate unique stride arguments for each buffer dimension.
+        stride_arg_count = 0
+        if use_dynamic_strides:
+            stride_arg_count = sum(
+                len(b.kernel_buffer_type.symbolic_shape)
+                for b in host_sig.buffer_bindings
+            )
+            if stride_arg_count > 0:
+                input_tensors += [IndexType.get()] * stride_arg_count
+
         if async_dispatch:
             fence_type = IrType.parse("!hal.fence")
             input_tensors += [fence_type] * 2
@@ -134,6 +145,8 @@ def isolated_test_call(
             sig.kernel_buffer_bindings + scalar_bindings
         )
 
+        if stride_arg_count > 0:
+            arg_locs += [Location.unknown()] * stride_arg_count
         if async_dispatch:
             arg_locs += [Location.unknown()] * 2
 
@@ -143,7 +156,7 @@ def isolated_test_call(
         dynamic_offset = scalars_offset + scalars_count
 
         with InsertionPoint(entry_block):
-            arguments = entry_block.arguments
+            arguments = list(entry_block.arguments)
             if async_dispatch:
                 in_fence = arguments[-2]
                 out_fence = arguments[-1]
@@ -181,6 +194,16 @@ def isolated_test_call(
                 arg_idx, dim_idx = arg_dim_mapping[symbol]
                 idx = arith_d.constant(IndexType.get(), dim_idx)
                 dynamic_argument_map[symbol] = tensor_d.dim(arguments[arg_idx], idx)
+
+            # Create workload base: scalars, then dynamic dims, then strides.
+            workload_base = scalars_args + [
+                dynamic_argument_map[dim] for dim in dynamic_symbols
+            ]
+            if stride_arg_count > 0:
+                stride_args = [to_index(v) for v in arguments[-stride_arg_count:]]
+                workload_base = workload_base + stride_args
+            else:
+                stride_args = []
 
             assert isinstance(entry_block, Block)
             # Create a flow.dispatch op to the kernel
@@ -237,8 +260,7 @@ def isolated_test_call(
 
                     out = flow_d.DispatchOp(
                         output_slices,
-                        [dynamic_argument_map[dim] for dim in dynamic_symbols]
-                        + scalars_args,
+                        workload_base,
                         entrypoints,
                         block_argument_list,
                         [dynamic_argument_map[dim] for dim in argument_dims],
@@ -264,8 +286,7 @@ def isolated_test_call(
                 # with the provided host signature arguments.
                 out = flow_d.DispatchOp(
                     memref_to_tensor(output_types),
-                    [dynamic_argument_map[dim] for dim in dynamic_symbols]
-                    + scalars_args,
+                    workload_base,
                     entrypoints,
                     list(arguments) + list(dynamic_argument_map.values()),
                     [dynamic_argument_map[dim] for dim in argument_dims],
