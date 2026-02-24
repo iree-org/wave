9663887a17ca543929ed134de9aad39fb9c71a1e
Ivan Butygin <ivan.butygin@gmail.com>
2026-02-24 13:26:39 +0100

    add pass
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- wave_lang/kernel/wave/asm/wave_asm/tools/waveasm-translate/waveasm-translate.cpp
+++ wave_lang/kernel/wave/asm/wave_asm/tools/waveasm-translate/waveasm-translate.cpp
@@ -101,6 +101,11 @@ static llvm::cl::opt<bool> runBufferLoadStrengthReduction(
     llvm::cl::init(false));
 
 static llvm::cl::opt<bool>
+    runLoopAddressPromotion("waveasm-loop-address-promotion",
+                            llvm::cl::desc("Run loop address promotion pass"),
+                            llvm::cl::init(false));
+
+static llvm::cl::opt<bool>
     emitAssembly("emit-assembly",
                  llvm::cl::desc("Emit AMDGCN assembly instead of MLIR"),
                  llvm::cl::init(false));
@@ -242,6 +247,12 @@ int main(int argc, char **argv) {
   // voffsets and increment by constant stride each iteration.
   if (runBufferLoadStrengthReduction) {
     pm.addPass(waveasm::createWAVEASMBufferLoadStrengthReductionPass());
+  }
+
+  // Loop address promotion: replace per-iteration v_add_u32 LDS address
+  // computation with precomputed rotating VGPR iter_args.
+  if (runLoopAddressPromotion) {
+    pm.addPass(waveasm::createWAVEASMLoopAddressPromotionPass());
   }
 
   // Memory offset optimization: fold constant address components into
