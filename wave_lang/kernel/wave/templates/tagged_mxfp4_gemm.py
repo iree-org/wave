f3d0a61288abff1c70f62c3c4a91d0c4f769e7db
Aurore De Spirlet <aurore.despirlet@gmail.com>
2026-02-27 19:13:34 +0000

    cleaning
    Signed-off-by: Aurore De Spirlet <aurore.despirlet@gmail.com>

--- wave_lang/kernel/wave/templates/tagged_mxfp4_gemm.py
+++ wave_lang/kernel/wave/templates/tagged_mxfp4_gemm.py
@@ -56,7 +56,7 @@ def get_tagged_mxfp4_gemm(
     Returns:
         (kernel_function, WaveCompileOptions)
     """
-    # Auto-select group_size_n (and whether reordering helps) if not specified.
+    # Auto-select group_size_n and determine whether reordering helps
     if reorder_workgroups:
         group_size_n, reorder_workgroups = compute_best_group_size_n(
             shape[0], shape[1], shape[2], block_shape[0], block_shape[1]
@@ -176,7 +176,7 @@ def get_tagged_mxfp4_gemm_preshuffle_b(
     Returns:
         (kernel_function, WaveCompileOptions)
     """
-    # Auto-select group_size_n (and whether reordering helps) if not specified.
+    # Auto-select group_size_n and determine whether reordering helps
     if reorder_workgroups:
         group_size_n, reorder_workgroups = compute_best_group_size_n(
             shape[0], shape[1], shape[2], block_shape[0], block_shape[1]
