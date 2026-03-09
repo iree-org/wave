cbfc258bdb175333b13d9b34396d17f5d307a537
Tim Gymnich <tim@gymni.ch>
2026-03-09 15:31:45 +0000

    [water] add --water-middle-end-lowering named pass pipeline
    Signed-off-by: Tim Gymnich <tim@gymni.ch>

---
+++ water/include/water/Pipelines.h
@@ -0,0 +1,16 @@
+//===- Pipelines.h - Water Named Pipelines ----------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef WATER_PIPELINES_H
+#define WATER_PIPELINES_H
+
+namespace mlir::water {
+void registerWaterPipelines();
+} // namespace mlir::water
+
+#endif // WATER_PIPELINES_H
