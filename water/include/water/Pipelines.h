28d9dd78006881aa262762950647539f5ec6df9e
Tim Gymnich <tim@gymni.ch>
2026-03-10 09:48:52 +0000

    fix copyright header
    Signed-off-by: Tim Gymnich <tim@gymni.ch>

--- water/include/water/Pipelines.h
+++ water/include/water/Pipelines.h
@@ -1,10 +1,8 @@
-//===- Pipelines.h - Water Named Pipelines ----------------------*- C++ -*-===//
+// Copyright 2026 The Wave Authors
 //
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
 
 #ifndef WATER_PIPELINES_H
 #define WATER_PIPELINES_H
