9ef56bf5588a97cb6b1d8a4c7a63b59f8e13912c
xintin <gaurav.verma@amd.com>
2026-04-08 05:13:21 +0000

    WIP: Fix vector arith.truncf f32->bf16 in WaveASM ArithHandlers
    Uses V_CVT_PK_BF16_F32 for vector truncf, matching the store handler's
    convertF32ToBF16ForStore logic. Still blocked on 365 vector mask ops
    (cmpi/andi/select on vector<4xindex/i1>) that WaveASM doesn't handle.

--- waveasm/lib/Transforms/handlers/ArithHandlers.cpp
+++ waveasm/lib/Transforms/handlers/ArithHandlers.cpp
@@ -539,7 +539,50 @@ LogicalResult handleArithTruncF(Operation *op, TranslationContext &ctx) {
   auto vecType = dyn_cast<VectorType>(srcType);
   int64_t numElems = vecType ? vecType.getNumElements() : 1;
 
-  if (numElems > 1) {
+  if (numElems > 1 && srcElemType.isF32() && dstElemType.isBF16()) {
+    Value srcVal = *src;
+    if (isAGPRType(srcVal.getType())) {
+      int64_t size = getRegSize(srcVal.getType());
+      auto vregType = ctx.createVRegType(size, size > 1 ? size : 1);
+      srcVal = V_ACCVGPR_READ_B32::create(builder, loc, vregType, srcVal);
+    }
+    Type srcRegType = srcVal.getType();
+
+    auto extractF32Elem = [&](int64_t i) -> Value {
+      if (auto pvreg = dyn_cast<PVRegType>(srcRegType)) {
+        int64_t baseIdx = pvreg.getIndex() + i;
+        auto elemType = PVRegType::get(builder.getContext(), baseIdx, 1);
+        return PrecoloredVRegOp::create(builder, loc, elemType, baseIdx, 1);
+      }
+      auto elemType = ctx.createVRegType(1, 1);
+      return ExtractOp::create(builder, loc, elemType, srcVal,
+                               builder.getI64IntegerAttr(i));
+    };
+
+    SmallVector<Value> packedVals;
+    for (int64_t i = 0; i < numElems; i += 2) {
+      Value elemI = extractF32Elem(i);
+      Value elemJ;
+      if (i + 1 < numElems) {
+        elemJ = extractF32Elem(i + 1);
+      } else {
+        auto immZero = ctx.createImmType(0);
+        elemJ = ConstantOp::create(builder, loc, immZero, 0);
+      }
+      auto vregType = ctx.createVRegType();
+      packedVals.push_back(
+          V_CVT_PK_BF16_F32::create(builder, loc, vregType, elemI, elemJ));
+    }
+
+    Value result;
+    if (packedVals.size() == 1) {
+      result = packedVals[0];
+    } else {
+      auto packedType = ctx.createVRegType(packedVals.size(), 1);
+      result = PackOp::create(builder, loc, packedType, packedVals);
+    }
+    ctx.getMapper().mapValue(truncOp.getResult(), result);
+  } else if (numElems > 1) {
     ctx.getMapper().mapValue(truncOp.getResult(), *src);
   } else {
     Value srcVal = *src;
